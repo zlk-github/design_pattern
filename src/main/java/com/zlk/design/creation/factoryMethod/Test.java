f3465562c2c9b6e2a47efca940297fe92010544c
unknown <2773407299@qq.com>
2020-07-12 17:59:36 +0800

    [feat]设计模式提交-1创建者模型；2结构型模型之适配器

---
+++ src/main/java/com/zlk/design/creation/factoryMethod/Test.java
@@ -0,0 +1,13 @@
+package com.zlk.design.creation.factoryMethod;
+
+import java.util.Calendar;
+
+public class Test {
+    public static void main(String[] args) {
+        PizzaStore1 pizzaStore1 = new PizzaStore1();
+        pizzaStore1.orderPizza();
+        System.out.println("----------------------------------");
+        PizzaStore2 pizzaStore2 = new PizzaStore2();
+        pizzaStore2.orderPizza();
+    }
+}
